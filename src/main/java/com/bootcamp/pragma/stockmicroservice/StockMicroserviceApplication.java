b0e4f5687aa0ac38b02d9030a2f2c8fba8590b43
Leandro Jacome <118925773+LeandroJacomeR@users.noreply.github.com>
2024-08-18 10:25:40 -0500

    Initial commit

---
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/StockMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.pragma.stockmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/pragma/stockmicroservice/StockMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.pragma.stockmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
